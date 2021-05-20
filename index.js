ffd373c771866e42869af22293b74f69873b2023
sam <surickj@gmail.com>
2021-05-20 10:48:10 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+ 
+function superbowlWin(array) {
+    const results = array.find(record => record.result === "W") 
+    return !!results ? results.year : undefined 
+    // if (results === true) {
+//         return results.year
+//     } else {
+//         return undefined
+//     }
+}
+
+//const superbowlWin = (array) => (array.find(array => array.result === "W"));
+
+
+// const superbowlWin = (array) => {
+//     return (array.find(array => array.result === "W"));
+// }
